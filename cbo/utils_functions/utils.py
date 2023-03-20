b358c01d0f1bc73388bc8f4502578b6a49111723
Theophile <theoph.champion@gmail.com>
2023-03-20 16:49:02 +0000

    document register_resolvers function

--- cbo/utils_functions/utils.py
+++ cbo/utils_functions/utils.py
@@ -101,6 +101,9 @@ def safe_add(dictionary, key, new_node):
 
 
 def register_resolvers():
+    """
+    Register all the hydra custom resolver
+    """
     OmegaConf.register_new_resolver("set_cost_value", lambda cost, c1: c1 if (1 < cost < 4) else 1)
     OmegaConf.register_new_resolver("set_variable_cost", lambda cost: cost >= 3)
     OmegaConf.register_new_resolver("get_values", lambda xs: [instantiate(xs[k]) for k in xs.keys()])
